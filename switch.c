5f723562f677186f46fab7677df1740640a3dc48
1spoorthigowda <spoorthigowda108@gmail.com>
2025-04-02 09:17:00 +0530

    print

---
+++ switch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    
+    printf("Select a program:\n");
+    printf("Press 1: BTech CSE\n");
+    printf("Press 2: BTech AIML\n");
+    printf("Press 3: BTech ESE\n");
+    printf("Press 4: MSE\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
